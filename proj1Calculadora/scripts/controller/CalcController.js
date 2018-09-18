7023b53e3fffb4df62646afb36b2816225c3bf5b
viniciusbeckerbernardini <vbkmma@gmail.com>
2018-09-18 08:11:16 -0300

    Primeiro commit

---
+++ proj1Calculadora/scripts/controller/CalcController.js
@@ -0,0 +1,26 @@
+class CalcController{
+	
+	constructor(){
+
+		this._displayCalc = "0";
+		this._currentDate;
+
+	}
+
+	get displayCalc(){
+		return this._displayCalc;
+	}
+
+	set displayCalc(value){
+		this._displayCalc = value;
+	}
+
+	get currentDate(){
+		return this._currentDate;
+	}
+
+	set currentDate(value){
+		this._currentDate = value;
+	}
+
+}
