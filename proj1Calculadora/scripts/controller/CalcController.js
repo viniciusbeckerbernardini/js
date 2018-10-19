c40eb600a2e839a45db68c4890578b9dadcf83a5
viniciusbeckerbernardini <vbkmma@gmail.com>
2018-10-19 10:51:39 -0300

    Corrigi os arquivos da calculadora, tinha colocado uma let errado

--- proj1Calculadora/scripts/controller/CalcController.js
+++ proj1Calculadora/scripts/controller/CalcController.js
@@ -4,7 +4,28 @@ class CalcController{
 
 		this._displayCalc = "0";
 		this._currentDate;
+		this.initialize();
 
+	}
+
+	initialize(){
+		// Let = Defini variáveis dentro do escopo
+		// EL = Convenção de element
+		let displayCalcEl = document.querySelector("#display");
+		let dateEl = document.querySelector("#data");
+		let timeEl = document.querySelector("#hora");
+
+		/*
+		Inner HTML insere é uma propriedade para manipular o dom, 
+		ele pega o objeto e insere uma informação dentro dele substituindo uo agregando a atual.
+		A informação é passada no formato html.
+		*/
+		// Agregando
+		// displayCalcEl.innerHTML += "45654";
+		// Substitiundo
+		displayCalcEl.innerHTML = "4567";
+		dateEl.innerHTML = "19/09/2018";
+		timeEl.innerHTML = "13:10";
 	}
 
 	get displayCalc(){
